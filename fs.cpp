bda666401b38c02f10d9aac71681d0ad37775de1
Acturus <viktorsavkin333@gmail.com>
2025-08-05 07:02:10 +0300

    second_commit

--- fs.cpp
+++ fs.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -8,6 +9,31 @@ void write_block(std::string block) {
   std::cout << "Block: " << block << "\n";
   // определить пустое место в memory
   // записать в него блок
+  std::fstream file;
+  file.open("memory", std::ios::in | std::ios::out);
+
+  char bufer[BLK_SIZE];
+  file.seekg(0, std::ios::beg);
+
+  while (file.read(bufer, BLK_SIZE)) {
+    bool is_empty = true;
+
+    for (int i = 0; i < BLK_SIZE; i++) {
+      if (bufer[i] != 0) {
+        is_empty = false;
+        break;
+      }
+    }
+
+    if (is_empty) {
+      int pos = (int)file.tellg() - BLK_SIZE;
+      file.seekp(pos);
+      file.write(block.c_str(), BLK_SIZE);
+      return; // или break, если нужно продолжить другие операции
+    }
+  };
+
+  file.close();
 }
 
 void write_file() {
