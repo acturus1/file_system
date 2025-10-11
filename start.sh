30cc388d127079dc9a1302e2942c6a0ddc343127
Acturus <viktorsavkin333@gmail.com>
2025-10-11 15:47:10 +0300

    edit start.sh
    Signed-off-by: Acturus <viktorsavkin333@gmail.com>

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 # g++ fs.cpp -o fs ; cat test/i_1.txt | ./fs write new_file ; rm fs
 
 if [ "$#" -eq 1 ] && [ "$1" = "d" ] ; then
-  g++ fs_daemon.cpp -g -o server.out 
-  g++ client.cpp -o client.out 
+  g++ ./daemon/fs_daemon.cpp -g -o server.out 
+  g++ ./client/client.cpp -o client.out 
   alacritty -e bash -c "./client.out ; exit" &
   gdb server.out
 else
-  g++ fs_daemon.cpp -o server.out 
-  g++ client.cpp -o client.out 
+  g++ ./daemon/fs_daemon.cpp -o server.out 
+  g++ ./client/client.cpp -o client.out 
   alacritty -e bash -c "./client.out ; exit" &
   ./server.out
 fi
