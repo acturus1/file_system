d2bfd75c2e742d91b23f5b4f0e1c4ba6799ac447
Acturus <viktorsavkin333@gmail.com>
2025-11-03 17:58:44 +0300

    Refactor read and constants code

--- daemon/constants.cpp
+++ daemon/constants.cpp
@@ -1,27 +1,6 @@
-#ifndef BLK_SIZE_CONSTANT
-#define BLK_SIZE_CONSTANT
+#include "constants.hpp"
 
 const int BLK_SIZE = 2;
-
-#endif
-
-#ifndef FAT_PATH_CONSTANT
-#define FAT_PATH_CONSTANT
-
 const char *FAT_PATH = "FAT";
-
-#endif
-
-#ifndef FIFO_PATH_CONSTANT
-#define FIFO_PATH_CONSTANT
-
 const char *fifo_path = "./myfifo";
-
-#endif
-
-#ifndef FIFO_PATH_CLIENT_CONSTANT
-#define FIFO_PATH_CLIENT_CONSTANT
-
 const char *fifo_path_client = "./server_fifo";
-
-#endif
