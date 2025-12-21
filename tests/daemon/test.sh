4b36924bc19ffcab64febdcd7df5f7318eac79dc
Acturus <viktorsavkin333@gmail.com>
2025-12-21 19:01:05 +0300

    Add ListTest

--- tests/daemon/test.sh
+++ tests/daemon/test.sh
@@ -1,4 +1,4 @@
 dd if=/dev/zero bs=1 of=test_memory count=1024 ;
 touch FAT ; 
-g++ ../../daemon/fs_daemon.cpp ../../tests/daemon/constants.cpp ../../daemon/utils/read.cpp ../../tests/daemon/write_test.cpp -lgtest -lgtest_main -pthread -o example_test ; 
+g++ ../../daemon/fs_daemon.cpp ../../tests/daemon/constants.cpp ../../daemon/utils/read.cpp ../../tests/daemon/tests.cpp -lgtest -lgtest_main -pthread -o example_test ; 
 ./example_test
