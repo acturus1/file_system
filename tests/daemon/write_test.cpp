3b7e85a9acb88490f94abecb68fe0cf9a1004d02
Acturus <viktorsavkin333@gmail.com>
2025-12-21 17:54:06 +0300

    Add EditTest and fixed error with edit

--- tests/daemon/write_test.cpp
+++ tests/daemon/write_test.cpp
@@ -1,5 +1,6 @@
 #include "../../daemon/model/fat_data.hpp"
 #include "../../daemon/utils/utils.hpp"
+#include <algorithm>
 #include <fstream>
 #include <gtest/gtest.h>
 #include <iostream>
@@ -10,6 +11,8 @@ void prepare_FAT(FATData &data);
 utils::Response utils::read_file(const char *filename, FATData &data,
                                  bool do_dir_check);
 int delete_file(const char *filename, FATData &data);
+int edit_file(const char *filename, const char *text, FATData &data,
+              bool do_user_checks = true);
 
 const char *file_path = "/f1";
 const char *file_content = "f1content";
@@ -36,6 +39,7 @@ TEST(WriteFileTest, CheckUpdateMemory) {
     }
   }
   ASSERT_EQ(result, std::string(file_path) + std::string(file_content));
+  memory_test.close();
 };
 
 TEST(ReadFileTest, CheckFATDataUpdatedAndMemory) {
@@ -47,6 +51,32 @@ TEST(ReadFileTest, CheckFATDataUpdatedAndMemory) {
 TEST(DeleteFileTest, CheckDeleteFileFromFat) {
   delete_file(file_path, data);
   EXPECT_EQ(data.files.find(file_path), data.files.end());
+};
+
+TEST(EditFileTest, EditFileTestFat) {
+  const char *new_content = "new_content";
+  write_file(file_path, file_content, data);
+  edit_file(file_path, "new_content", data);
+  std::ifstream memory_test("./test_memory", std::ios::binary);
+
+  char output[100];
+  memory_test.read(output, sizeof(output));
+  std::string result;
+  for (int i = 0; i < 100; i++) {
+    if (output[i] != '\0') {
+      result += output[i];
+    }
+  }
+  std::string combined = std::string(file_path) + std::string(new_content);
+
+  std::string sorted_result = result;
+  std::string sorted_combined = combined;
+
+  std::sort(sorted_result.begin(), sorted_result.end());
+  std::sort(sorted_combined.begin(), sorted_combined.end());
+
+  ASSERT_EQ(sorted_result, sorted_combined);
+  memory_test.close();
 };
 
 int main(int argc, char **argv) {
