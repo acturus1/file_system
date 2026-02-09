a4ad11830e72fe797898830eed00b28afff7c452
Acturus <viktorsavkin333@gmail.com>
2026-02-09 19:20:56 +0300

    Add move func (main.cpp) and cleanup (mod_start.sh)

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -60,6 +60,10 @@ int main() {
         list_files(absolute_file_path.c_str(), data);
       } else if (buffer[0] == 't') {
         cout_recursive(data, absolute_file_path, 0);
+      } else if (buffer[0] == 'v') {
+        std::string filepath_new;
+        iss >> filepath_new;
+        move_file(data, filepath_new.c_str(), absolute_file_path.c_str());
       }
       if (buffer[bytes_read - 1] != '\n') {
         std::cout << std::endl;
